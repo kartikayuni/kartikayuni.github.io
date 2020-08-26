117f5af416de1767a5df091a9e38c76d67b2d5fd
Yuni <learnbahasawith.yuni@gmail.com>
2020-08-26 22:04:01 +0700

    third commit

--- hamburger.js
+++
@@ -1,9 +0,0 @@
-function menuToggle(){
-    var nbar = document.getElementById('topnav');
-    if(nbar.className === 'top'){
-        nbar.className += ' expand';
-    }
-    else{
-        nbar.className = 'top';
-    }
-}
